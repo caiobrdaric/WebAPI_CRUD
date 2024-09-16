961b2a92d25e6954a438f8653ffe9aa41e579847
Caio Brdaric <caiobrdaric@gmail.com>
2024-09-16 16:05:36 -0300

    empty

--- SistemaDeRegistros/obj/Debug/net8.0/SistemaDeRegistros.AssemblyInfo.cs
+++ SistemaDeRegistros/obj/Debug/net8.0/SistemaDeRegistros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeRegistros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d593607192c236afe371e5975499d55f7ffa178")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeRegistros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeRegistros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
